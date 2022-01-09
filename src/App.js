04312e576cae72e3fb50b23429e273d501f4743c
Matheus <kamaitatsu@gmail.com>
2022-01-09 01:58:20 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './App.css';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <header>
+        <Header />
+        <section>
+          <p>Sistema Solar</p>
+        </section>
+      </header>
+    );
   }
 }
 // aaa
