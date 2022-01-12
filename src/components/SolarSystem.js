d2f2ece88f4b36960b713853610dbbd7e8537a13
Matheus <kamaitatsu@gmail.com>
2022-01-12 19:15:06 -0300

    pull

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Title from './Title';
-// import planets from '../data/planets';
-// import PlanetCard from './PlanetCard';
-
-// const cadaPlaneta = planets
-//   .map((planeta, index) => (<li key={index}>{planeta.name}</li>));
-// return (
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const eachPlanet = planets
+      .map((planeta, index) => (<PlanetCard planetName={ planeta.props.children.name } planetImage={ planeta.props.children.image }  />));
+      // console.log(eachName[0].props.children.name);
     return (
+
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {eachPlanet}
+        </ul>
       </div>
     );
   }
